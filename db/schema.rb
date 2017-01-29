f46963ece2e3a67b42cc242c1273c3676e599914
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-01-29 10:44:27 +0900

    modify column

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170127151952) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "title",      null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
