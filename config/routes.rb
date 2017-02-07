cef33dfd0f82eec5d84be0a090449438838d6715
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-07 16:42:13 +0900

    %i action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, except: %i( index show destroy )
   root  'messages#index'
-  resources :groups, except: [:show, :destroy]
+  resources :groups, except: %i( show destroy )
 end
