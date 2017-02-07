ede4f6253787510e962c069730f5def92bcdb51c
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-07 15:53:19 +0900

    rails g controller users

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 
+  before_action :set_group, only: %i( edit update )
+
   def index
     @groups = Groups.all
   end
@@ -13,21 +15,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(id_params)
   end
 
   def update
-    @group = Group.find(id_params)
     @group.update(group_params)
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name)
   end
 
-  def id_params
-    params.permit(:id)
+  def set_group
+    @group = Group.find(params[:id])
   end
 
 end
