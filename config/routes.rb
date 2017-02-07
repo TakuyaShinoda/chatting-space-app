f26cdeaefe20ed075e6e9fd224d9a96b4ee15982
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-07 17:00:48 +0900

    modify routing of devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, except: %i( index show destroy )
+  devise_for :users, only: %i( session registration )
   root  'messages#index'
   resources :groups, except: %i( show destroy )
 end
