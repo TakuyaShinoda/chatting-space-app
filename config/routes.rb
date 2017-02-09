f5edefef7c75966aab85a7dbe9bbef879ac6eae4
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-10 02:25:43 +0900

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, only: %i( session registration )
-  resources :groups, except: %i( show destroy ) do
-  	resources :messages, only: %i( index new create )
-  end
   root  'groups#index'
+  resources :groups, except: %i( show destroy ) do
+    resources :messages, only: %i( index new create )
+  end
 end
