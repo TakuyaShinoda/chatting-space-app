075a503cff842b9b77ff535a63729b779e356f5e
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-09 19:41:17 +0900

    modify indent

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,8 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-  	@groups = current_user.groups
-  	@group = Group.find(params[:group_id])
+    @groups = current_user.groups
+    @group = Group.find(params[:group_id])
   end
 
 end
