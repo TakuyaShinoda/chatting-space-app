3850f5580d708a1c6073ec6546616b5efca2148b
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-12 17:58:55 +0900

    flash message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,12 +6,17 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @messages = Message.where(group_id: params[:group_id])
     @message = Message.new
+    @last_message = @messages.last
   end
 
   def create
     @message = @group.messages.new(message_params)
-    @message.save
-    redirect_to group_messages_path
+    if @message.body.blank?
+      redirect_to group_messages_path, alert: 'メッセージが投稿できません。'
+    else
+      @message.save
+      redirect_to group_messages_path
+    end
   end
 
   private
