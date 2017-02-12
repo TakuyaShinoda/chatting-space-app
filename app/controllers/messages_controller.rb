447a5bcf498cdf95c43ef2c5fc67de9d63f57cf1
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-12 16:05:52 +0900

    modify message_controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,27 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_group, only: %i( index create )
 
   def index
     @groups = current_user.groups
-    @group = Group.find(params[:group_id])
     @messages = Message.where(group_id: params[:group_id])
     @message = Message.new
-    @last_message = Message.find_by(group_id: params[:group_id])
   end
 
-  def new; end
-
   def create
-  	Message.create(message_params)
+    @message = @group.messages.new(message_params)
+    @message.save
     redirect_to group_messages_path
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 
 end
