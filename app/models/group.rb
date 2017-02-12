579628871549d91efe33a4ee380290b2084e3c8d
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-12 15:43:41 +0900

    modify message model

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,10 @@ class Group < ApplicationRecord
 
   has_many :group_users
   has_many :users, through: :group_users
+  has_many :messages
   accepts_nested_attributes_for :users
+
+  def last_message
+  	Message.last.body
+  end
 end
