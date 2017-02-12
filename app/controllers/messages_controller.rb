d7b2790b913e2ad78df79d4d3399167145704593
篠田拓也 <Takuya@Takuya-no-MacBook-Air.local>
2017-02-12 11:36:26 +0900

    messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,21 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
+    @messages = Message.where(group_id: params[:group_id])
+    @message = Message.new
+    @last_message = Message.find_by(group_id: params[:group_id])
+  end
+
+  def new; end
+
+  def create
+  	Message.create(message_params)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 
 end
